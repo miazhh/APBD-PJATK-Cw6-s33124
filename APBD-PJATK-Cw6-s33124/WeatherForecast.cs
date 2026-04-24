a6d8497d989cbb5c0617414582ae194759c21a9c
Hubert Miaz <s33124@pjwstk.edu.pl>
2026-04-24 14:59:45 +0200

    Inicjacja projektu

---
+++ APBD-PJATK-Cw6-s33124/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace APBD_PJATK_Cw6_s33124;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
